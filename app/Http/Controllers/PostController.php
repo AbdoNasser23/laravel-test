e5670079b1f7e7f3eb0a840232ca9125dab51e2e
AbdoNasser <93087622+AbdoNasser23@users.noreply.github.com>
2026-08-27 23:49:41 +0300

    The End

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,6 @@ use App\Http\Requests\PostRequest;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate;
 
 class PostController extends Controller
 {
